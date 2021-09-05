0a4c9b76f5ea5c91ad15324b2427610c908b620e
LAUKIK07 <88126412+LAUKIK07@users.noreply.github.com>
2021-09-05 21:46:39 +0530

    Add files via upload

---
+++ 8-Left ROTATION.cpp
@@ -0,0 +1,52 @@
+#include <map>
+#include <set>
+#include <list>
+#include <cmath>
+#include <ctime>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <string>
+#include <bitset>
+#include <cstdio>
+#include <limits>
+#include <vector>
+#include <climits>
+#include <cstring>
+#include <cstdlib>
+#include <fstream>
+#include <numeric>
+#include <sstream>
+#include <iostream>
+#include <algorithm>
+#include <unordered_map>
+
+using namespace std;
+
+vector<int> array_left_rotation(vector<int> a, int n, int k) {
+    int tmp=0,x=0;
+    while(x < k){
+        tmp = a[0];
+        for(int i=0; i < n; i++){
+            a[i]=a[i+1];
+            if(i+1 == n) a[i]= tmp;
+        }
+        x++;
+    }
+    return a;
+}
+
+int main(){
+    int n;
+    int k;
+    cin >> n >> k;
+    vector<int> a(n);
+    for(int a_i = 0;a_i < n;a_i++){
+        cin >> a[a_i];
+    }
+    vector<int> output = array_left_rotation(a, n, k);
+    for(int i = 0; i < n;i++)
+        cout << output[i] << " ";
+    cout << endl;
+    return 0;
+}
